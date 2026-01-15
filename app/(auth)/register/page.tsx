5ea75c67be7bed782f5b822963185755f2c558b9
Edward Melendez <edward.melendez@limber.io>
2026-01-15 13:24:38 -0500

    upd: register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { RegisterForm } from '@/components/auth/register-form';
+// import { RegisterForm } from '@/components/auth/register-form';
+import {redirect} from "next/navigation";
 
 export default function RegisterPage() {
-  return <RegisterForm />;
+  // return <RegisterForm />;
+  return redirect('login')
 }
